e97d78351f2dd887f06e5805d0484940dd91892f
Maksym Koban <dnc.maxim.koban@gmail.com>
2023-05-25 21:58:57 +0300

    init commit

---
+++ src/Services/Attribute/AttributeResolver.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Attribute;
+
+class AttributeResolver
+{
+    public function resolveAttributesByDictionary(array $dictionary, string $slug): array
+    {
+        // some action for resolve attributes;
+        $result = [
+            'iphone-14-pro-max',
+            'iphone-14-pro',
+            'iphone-13-pro',
+        ];
+
+        return $result;
+    }
+}
---
+++ tests/AttributeResolverTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Do not modify!
+ */
+class AttributeResolverTest extends KernelTestCase
+{
+    protected function setUp(): void
+    {
+        self::bootKernel();
+    }
+
+    /**
+     * @param array $dictionary
+     * @param string $slug
+     * @param array $expectedResult
+     *
+     * @dataProvider provideData
+     */
+    public function testGetDataAttributesFromStringAndDictionary(
+        array $dictionary,
+        string $slug,
+        array $expectedResult,
+    ): void {
+        $attributeResolver = $this->getContainer()->get('App\Services\Attribute\AttributeResolver');
+        $actual = $attributeResolver->resolveAttributesByDictionary($dictionary, $slug);
+
+        sort($actual);
+        sort($expectedResult);
+
+        $this->assertEquals(implode(',', $expectedResult), implode(',', $actual));
+    }
+
+    public function provideData(): iterable
+    {
+        $dictionary = [
+            'iphone-11',
+            'iphone-12',
+            'iphone-13',
+            'iphone-14',
+            'iphone-11-pro',
+            'iphone-12-pro',
+            'iphone-13-pro',
+            'iphone-14-pro',
+            'iphone-11-pro-max',
+            'iphone-12-pro-max',
+            'iphone-13-pro-max',
+            'iphone-14-pro-max',
+            'iphone-se',
+        ];
+
+        return [
+            'Test #1' => [
+                $dictionary,
+                'iphone-14-pro-max-iphone-14-pro-iphone-13-pro',
+                [
+                    'iphone-14-pro-max',
+                    'iphone-14-pro',
+                    'iphone-13-pro',
+                ],
+            ],
+            'Test #2' => [
+                $dictionary,
+                'iphone-14-iphone-14-pro-iphone-13',
+                [
+                    'iphone-14',
+                    'iphone-14-pro',
+                    'iphone-13',
+                ],
+            ],
+            'Test #3' => [
+                $dictionary,
+                'iphone-14-pro-iphone-14-pro-max-iphone-13-pro-max',
+                [
+                    'iphone-14-pro',
+                    'iphone-14-pro-max',
+                    'iphone-13-pro-max',
+                ],
+            ],
+        ];
+    }
+}
