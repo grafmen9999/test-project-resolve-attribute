e5b2724abaf028e13d9871c375895f8c8165c003
Maksym Koban <dnc.maxim.koban@gmail.com>
2023-05-29 23:53:02 +0300

    Refactoring and update code-style

--- src/Services/Attribute/AttributeResolver.php
+++ src/Services/Attribute/AttributeResolver.php
@@ -6,35 +6,29 @@ namespace App\Services\Attribute;
 
 class AttributeResolver
 {
-    public function resolveAttributesByDictionary(array $dictionary, string $slug): array
+    public function resolveAttributesByDictionary(array $dictionary, string $slug, string $separator = '-'): array
     {
-        // some action for resolve attributes;
         $result = [];
-
-        $explodedSlug = explode('-', $slug);
 
         $lastKey = null;
         $currentSlugArray = [];
 
-        foreach ($explodedSlug as $itemSlug) {
-
+        foreach (explode($separator, $slug) as $itemSlug) {
             $currentSlugArray[] = $itemSlug;
-            $currentSlug = implode('-', $currentSlugArray);
+            $currentSlug = implode($separator, $currentSlugArray);
 
             $key = array_search($currentSlug, $dictionary);
 
-            if (false === $key) {
-                if (null === $lastKey) {
-                    continue;
-                }
-
-                $result[] = $dictionary[$lastKey];
-                $currentSlugArray = [$itemSlug];
-                $lastKey = null;
+            if (false !== $key) {
+                $lastKey = $key;
                 continue;
             }
 
-            $lastKey = $key;
+            if (null !== $lastKey) {
+                $result[] = $dictionary[$lastKey];
+                $currentSlugArray = [$itemSlug];
+                $lastKey = null;
+            }
         }
 
         if (null !== $lastKey) {
